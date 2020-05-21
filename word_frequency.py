7de25b18c7b36c241b453e01ef28eee87435a1a5
Sarah <sarah.akus@gmail.com>
2020-05-21 10:40:25 -0400

    final:

--- word_frequency.py
+++ word_frequency.py
@@ -18,16 +18,12 @@ def print_word_freq(file):
         if character not in punctuation:
             nopunc = nopunc + character
     lower_text = nopunc.lower()
-    # print(lower_text)
     no_whitespace = lower_text.split()
-    # print(no_whitespace)
 
     for word in no_whitespace:
         if word not in stop_words:
             found_words.append(word)
-    # print(found_words)
     sort_words = sorted(found_words)
-    # print(sort_words)
     duplicate_words = dict()
 
     for word in sort_words:
@@ -35,10 +31,9 @@ def print_word_freq(file):
             duplicate_words[word] += 1
         else:
             duplicate_words[word] = 1
-    duplicate_words = { key:value for key, value in duplicate_words.items() if value >= 1}
 
     for key, value in duplicate_words.items():
-        print(key , ' | ', value, value * ("*"))
+        print(key.rjust(15) , ' | ', str(value).center(5), value * ("*"))
 
     
     
